96f35ddd1933b6da4136cda053bedb04a0af1b0a
Sidge4real <vanderspiegel.lukas@gmail.com>
2024-07-07 11:12:10 +0200

    initial commit

---
+++ src/main/java/com/example/doolhof/DoolhofApplication.java
@@ -0,0 +1,13 @@
+package com.example.doolhof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoolhofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoolhofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doolhof/DoolhofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doolhof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoolhofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
